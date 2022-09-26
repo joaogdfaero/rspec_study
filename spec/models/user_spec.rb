1eb7dfbc586589cdf1e758214837949125bc7e9a
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-26 18:30:06 -0300

    corrigido erro no :wizard do user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    expect(User.create(nickname: "Chronos", kind: wizard, level: 100)).to_not be_valid
+    expect(User.create(nickname: "Chronos", kind: :wizard, level: 100)).to_not be_valid
   end
   it "returns the correct hero title"
 end
