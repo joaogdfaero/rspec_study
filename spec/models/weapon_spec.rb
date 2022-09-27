772acb8a28ba273faab071bc7ace02a8fed6c6da
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-27 08:51:40 -0300

    3 tests working without FFaker and Factorybot

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Weapon, type: :model do
   it "is valid if the level is between 1 and 99" do
     expect(Weapon.create(name: "Faquinha", description: "Faca", power_base: 200, power_step: 150, level:14 )).to be_valid
   end
-  it "is invalid if current_power is negative"
-  it "it returns the correct weapon title"
+  it "is invalid if power_base is negative" do
+    expect(Weapon.create(name: "Faquinha", description: "Faca", power_base: -20, power_step: 150, level:14 )).to_not be_valid
+  end
+  it "it returns the correct weapon title" do
+    weapon = Weapon.create(name: "Faquinha", description: "Faca", power_base: 200, power_step: 150, level:14 )
+    expect(weapon.title).to eq ("Faquinha #14")
+  end
 end
