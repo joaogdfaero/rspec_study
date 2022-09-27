39a3522f61c6ff8d9b3c4d8a6ce56188bf3f81b3
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-27 08:47:20 -0300

    level test weapon worked

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,4 +1,10 @@
 class Weapon < ApplicationRecord
+    validates :level, numericality: { greater_than:0, less_than_or_equal_to:99}
+    validates :power_base, numericality: { greater_than:0}
+    validates :power_step, numericality: { greater_than:0, less_than_or_equal_to:1000}
+    validates :current_power, numericality: { greater_than:0}
+
+
     def current_power
         current_power = (power_base+((level-1)*power_step))
     end
--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Weapon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid if the level is between 1 and 99" do
+    expect(Weapon.create(name: "Faquinha", description: "Faca", power_base: 200, power_step: 150, level:14 )).to be_valid
+  end
+  it "is invalid if current_power is negative"
+  it "it returns the correct weapon title"
 end
