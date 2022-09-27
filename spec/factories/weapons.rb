3f215c9704b09025667b9b82606608309346a9bf
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-27 10:13:13 -0300

    finished challenge with everything working

--- spec/factories/weapons.rb
+++ spec/factories/weapons.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :weapon do
-    
+    name {FFaker::Lorem.word}
+    description {FFaker::Lorem.word}
+    power_base { FFaker::Random.rand(1..9999) }
+    power_step { FFaker::Random.rand(1..1000) }
+    level { FFaker::Random.rand(1..99) }
   end
 end
--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Weapon, type: :model do
   it "is valid if the level is between 1 and 99" do
-    expect(Weapon.create(name: "Faquinha", description: "Faca", power_base: 200, power_step: 150, level:14 )).to be_valid
+    weapon = build(:weapon, level: FFaker::Random.rand(1..99))
+    expect(weapon).to be_valid
   end
   it "is invalid if power_base is negative" do
-    expect(Weapon.create(name: "Faquinha", description: "Faca", power_base: -20, power_step: 150, level:14 )).to_not be_valid
+  weapon = build(:weapon, power_base: FFaker::Random.rand(-9999..0))
+  expect(weapon).to_not be_valid
   end
   it "it returns the correct weapon title" do
-    weapon = Weapon.create(name: "Faquinha", description: "Faca", power_base: 200, power_step: 150, level:14 )
-    expect(weapon.title).to eq ("Faquinha #14")
+    name = FFaker::Name.first_name
+    level = FFaker::Random.rand(1..99)
+    weapon = build(:weapon, name: name, level: level)
+    expect(weapon.title).to eq ("#{name} ##{level}")
   end
 end
